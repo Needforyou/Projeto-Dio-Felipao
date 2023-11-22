80133155667a100b0fde90d93b5010cf7df4712e
Needforyou <igor.nobre.bass@gmail.com>
2023-11-22 17:29:23 -0300

    Projeto Concluido

---
+++ index.js
@@ -0,0 +1,34 @@
+let nomeDoHeroi = "Barba"
+let quantidadexp = 2000
+
+if (quantidadexp <= 1000){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP ferro")
+}
+
+else if (quantidadexp >= 1001 && quantidadexp <= 2000){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP Bronze")
+}
+
+else if (quantidadexp >= 2001 && quantidadexp <= 5000){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP Prata")
+}
+
+else if (quantidadexp >= 6001 && quantidadexp <= 7000){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP Ouro")
+}
+
+else if (quantidadexp >= 7001 && quantidadexp <= 8000){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP Platina")
+}
+
+else if (quantidadexp >= 8001 && quantidadexp <= 9000){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP Ascendente")
+}
+
+else if (quantidadexp >= 9001 && quantidadexp <= 10000){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP Imortal")
+}
+
+else if (quantidadexp >= 10001){
+    console.log ("O herói " + nomeDoHeroi + " está no nível de XP Radiante")
+}
